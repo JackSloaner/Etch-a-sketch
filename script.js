86634debf833cdacbda4a8da6a4592019a3da529
Jack Sloan <jack12sloan@gmail.com>
2022-10-22 10:55:38 -0400

    Create drawing effect for 16x16 grid

--- script.js
+++ script.js
@@ -3,5 +3,16 @@ const container = document.querySelector('.grid')
 for (let i = 0; i < 256; i++){
     const newDiv = document.createElement('div');
     newDiv.classList.add('box');
+    newDiv.addEventListener('mouseover', changeColor)
     container.appendChild(newDiv);
+}
+
+let hovercolor = 'grey'
+
+document.addEventListener('mousedown', () => {hovercolor = 'white';})
+document.addEventListener('mouseup', () => {hovercolor = 'grey'})
+
+function changeColor(e){
+    const box = e.target;
+    box.style.backgroundColor = hovercolor;
 }
