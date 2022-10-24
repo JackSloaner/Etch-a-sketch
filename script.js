7bd8c05316bad65e7e85f8262d375b7dd242300e
Jack Sloan <jack12sloan@gmail.com>
2022-10-24 12:43:53 -0400

    Add button that changes grid size

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
+const newGridBtn = document.querySelector('.newGrid')
+newGridBtn.addEventListener('click', getGridDimensions)
 const container = document.querySelector('.grid')
-
-for (let i = 0; i < 256; i++){
-    const newDiv = document.createElement('div');
-    newDiv.classList.add('box');
-    newDiv.addEventListener('mouseover', changeColor)
-    container.appendChild(newDiv);
-}
 
 let hovercolor = 'grey'
 
 document.addEventListener('mousedown', () => {hovercolor = 'white';})
 document.addEventListener('mouseup', () => {hovercolor = 'grey'})
 
+
+function makeGrid(length){
+container.style.gridTemplateColumns = `repeat(${length}, 1fr)`;
+let numSquares = length ** 2;
+removeGrid();
+for (let i = 0; i < numSquares; i++){  
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('box');
+    newDiv.addEventListener('mouseover', changeColor)
+    container.appendChild(newDiv);
+}
+}
+
+function removeGrid(){
+    let boxes = document.querySelectorAll('.box')
+    boxes.forEach(box => {
+        container.removeChild(box);
+    });
+}
+
 function changeColor(e){
     const box = e.target;
     box.style.backgroundColor = hovercolor;
 }
+
+function getGridDimensions(){
+    let newSideLength = prompt('Choose new grid size: what is the new length of the sides?')
+    if (newSideLength > 0 && newSideLength <= 100) {
+        makeGrid(newSideLength);
+    } else if (newSideLength) {
+        alert('length must be between 1 and 100!');
+        getGridDimensions();
+    }
+}
+
+makeGrid(16)
